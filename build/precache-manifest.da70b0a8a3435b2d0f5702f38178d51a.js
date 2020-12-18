d53ef85a60f97393f1167bb7b88ad54c7f6fc882
viniclefer <viniciusclementefernandes@gmail.com>
2020-12-18 10:00:23 -0300

    create build

---
+++ build/precache-manifest.da70b0a8a3435b2d0f5702f38178d51a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec7f302158c44efccc1bc7949c39f79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26f5932da11efc8c65ec",
+    "url": "/static/css/2.4949f480.chunk.css"
+  },
+  {
+    "revision": "f3d0393b87d642980484",
+    "url": "/static/css/main.ad3a7718.chunk.css"
+  },
+  {
+    "revision": "26f5932da11efc8c65ec",
+    "url": "/static/js/2.4a7ec3c7.chunk.js"
+  },
+  {
+    "revision": "ddf08bf02da6e3b30b57bcd0d409473f",
+    "url": "/static/js/2.4a7ec3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d0393b87d642980484",
+    "url": "/static/js/main.0015cd8c.chunk.js"
+  },
+  {
+    "revision": "a0f4ed99b95d5839e5b9",
+    "url": "/static/js/runtime-main.8b9ffed5.js"
+  },
+  {
+    "revision": "a5179a5d4dcdb93bec8ba8a6146c65c6",
+    "url": "/static/media/logo.a5179a5d.png"
+  }
+]);
